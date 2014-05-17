a22da94144fe0f9ece3840c3f545a8fe47601e07
SevereOverfl0w <overfl0w@narna.co>
2014-05-17 09:50:36 +0100

    Thank god for gg=G

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,17 @@
 ko.bindingHandlers.editableText = {
-init: function(element, valueAccessor) {
-$(element).attr('contenteditable', true);
-$(element).on('blur', function() {
-var observable = valueAccessor();
-if (typeof (observable) == 'function'){
-observable( $(this).text() );
-}
-});
-},
-update: function(element, valueAccessor) {
-var value = ko.utils.unwrapObservable(valueAccessor());
-$(element).text(value);
-}
+    init: function(element, valueAccessor) {
+        $(element).attr('contenteditable', true);
+        $(element).on('blur', function() {
+            var observable = valueAccessor();
+            if (typeof (observable) == 'function'){
+                observable( $(this).text() );
+            }
+        });
+    },
+    update: function(element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        $(element).text(value);
+    }
 };
 
 function AppViewModel(){
